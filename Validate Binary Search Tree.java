118c5162799aeb14d1b4b9a2c4bbf0c5451efa64
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2022-08-11 21:08:21 +0530

    Update Validate Binary Search Tree.java

--- Validate Binary Search Tree.java
+++ Validate Binary Search Tree.java
@@ -15,13 +15,17 @@
  */
 public class Solution {
 public boolean isValidBST(TreeNode root) {
+    //for empty tree
     return isValid(root, null, null);
 }
 
 public boolean isValid(TreeNode root, Integer min, Integer max) {
-    if(root == null) return true;
-    if(min != null && root.val <= min) return false;
-    if(max != null && root.val >= max) return false;
+    if(root == null) 
+        return true;
+    if(min != null && root.val <= min) 
+        return false;
+    if(max != null && root.val >= max) 
+        return false;
     
     return isValid(root.left, min, root.val) && isValid(root.right, root.val, max);
     }
