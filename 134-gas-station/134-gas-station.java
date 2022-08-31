479a0e200651c04513cc00daf8cd951b48804014
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2022-08-31 11:42:13 +0530

    Time: 3 ms (65.76%), Space: 80.2 MB (64.99%) - LeetHub

--- 134-gas-station/134-gas-station.java
+++ 134-gas-station/134-gas-station.java
@@ -3,18 +3,9 @@ class Solution {
         public int canCompleteCircuit(int[] gas, int[] cost) {
         int total=0;
         int ans=0;
+            int tank=0;
         for (int i = 0; i < cost.length; i++) {
             total+=(gas[i]-cost[i]);
-            
-        }
-
-        if(total<0)
-        {
-            return -1;
-        }
-        int tank=0;
-        for(int i=0;i<gas.length;i++)
-        {
             tank+=gas[i]-cost[i];
             if(tank<0)
             {
@@ -23,6 +14,12 @@ class Solution {
             }
             
         }
+
+        if(total<0)
+        {
+            return -1;
+        }
+        
         return ans;
         
     }
