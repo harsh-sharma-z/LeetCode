e6ad627fa02c682f77ffadcfb828b664bfb3ecb9
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2022-08-31 13:57:31 +0530

    Time: 2 ms (89.42%), Space: 50.4 MB (94.31%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -13,13 +13,13 @@ class Solution {
             pr*=nums[n-i-1];
         }
 
-        int ans[]=new int[n];
+        
 
 
         for(int i=0;i<n;i++)
-        ans[i]=left[i]*right[i];
+        nums[i]=left[i]*right[i];
 
-        return ans;
+        return nums;
         
         
     }
