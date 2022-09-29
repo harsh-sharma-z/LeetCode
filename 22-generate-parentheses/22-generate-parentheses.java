eedc7e0faeea0997f0a6af03469d3985fe0bf286
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2022-09-29 14:04:18 +0530

    Time: 2 ms (71.26%), Space: 44.1 MB (32.31%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -1,62 +1,19 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        String s="";
-        for(int i=0;i<n;i++)
-        {
-            s=s+"()";
-        }
         List<String> uwu=new ArrayList<>();
         String ans="";
-        print(s,ans,uwu);
+        help(n,0,0,ans,uwu);
         return uwu;
     }
     
-    public void print(String ques, String ans,List<String> uwu) {
-        if (ques.length() == 0) {
-            if(isValid(ans))
-            uwu.add(ans);
-            return;
-        }
-
-        for (int i = 0; i < ques.length(); i++) {
-            char ch = ques.charAt(i);
-            boolean f = false;
-            for (int j = i + 1; j < ques.length(); j++) {
-                if (ques.charAt(j) == ch) {
-                    f = true;
-                    break;
-                }
-            }
-            if (f == false) {
-                String s1 = ques.substring(0, i);
-                String s2 = ques.substring(i + 1);
-                print(s1 + s2, ans + ch,uwu);
-            }
-        }
-    }
-    
-    public boolean isValid(String s)
+    public void help(int n,int open,int close,String ans,List<String>uwu)
     {
-        HashMap<Character, Character> map = new HashMap<Character, Character>();
-	map.put('(', ')');
-	
- 
-	Stack<Character> stack = new Stack<Character>();
- 
-	for (int i = 0; i < s.length(); i++) {
-		char curr = s.charAt(i);
- 
-		if (map.keySet().contains(curr)) {
-			stack.push(curr);
-		} else if (map.values().contains(curr)) {
-			if (!stack.empty() && map.get(stack.peek()) == curr) {
-				stack.pop();
-			} else {
-				return false;
-			}
-		}
-	}
- 
-	return stack.empty();
+        if(open==n && close == n)
+            uwu.add(ans);
+        if(open<n)
+            help(n,open+1,close,ans+"(",uwu);
+        
+        if(close<open)
+            help(n,open,close+1,ans+")",uwu);
     }
 }
