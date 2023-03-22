2ba5fce148ef6ce69547ab8e91a3ba42112bcbfa
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2023-03-22 14:48:35 +0530

    Time: 1 ms (99.41%), Space: 43.7 MB (17.96%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,51 +1,28 @@
 class Solution {
     public int findCircleNum(int[][] arr) {
-        ArrayList<ArrayList<Integer>> uwu=new ArrayList<>();
-        
-        
-        for(int i=0;i<arr.length;i++)
-        {
-            uwu.add(new ArrayList<Integer>());
-        }
-        
-        for(int i=0;i<arr.length;i++)
-        {
-            for(int j=0;j<arr[i].length;j++)
-            {
-                if(arr[i][j]==1 && i!=j)
-                {
-                    uwu.get(i).add(j);
-                    uwu.get(j).add(i);
-                }
-            }
-        }
-        
-        
-        int vis[]=new int[arr.length];
-        int ans=0;
-        
-        
-        for(int i=0;i<vis.length;i++)
+        int n=arr.length;
+        int vis[]=new int[n];
+        int c=0;
+        for(int i=0;i<n;i++)
         {
             if(vis[i]==0)
             {
-                ans++;
-                dfs(i,uwu,vis);
+                dfs(i,n,arr,vis);
+                c++;
             }
         }
-        
-        return ans;
+        return c;
     }
     
-    
-    private void dfs(int node, ArrayList<ArrayList<Integer>> uwu, int[] vis)
-    {
-        vis[node]=1;
+    private void dfs(int src, int n , int[][] arr , int[] vis)
+    {   
         
-        for(Integer i: uwu.get(node))
+        for(int i=0;i<n;i++)
         {
-            if(vis[i]==0)
-                dfs(i,uwu,vis);
+            if(arr[src][i]==1&&vis[i]==0){
+                vis[i]=1;
+                dfs(i,n,arr,vis);
+            }
         }
     }
 }
