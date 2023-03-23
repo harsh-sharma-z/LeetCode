4c884c84d0f2080b6fa128358e0e30702c642301
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2023-03-23 09:46:31 +0530

    Time: 8 ms (28.80%), Space: 43.2 MB (36.54%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,25 +1,59 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int[] prerequisite = new int[numCourses];
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        for(int[] arr : prerequisites){
-            prerequisite[arr[0]]++;
-            graph.computeIfAbsent(arr[1], list -> new ArrayList<>()).add(arr[0]);
+    public boolean canFinish(int n, int[][] pre) {
+        HashMap<Integer,List<Integer>> map=new HashMap<>();
+        
+        for(int i=0;i<n;i++)
+        {
+            map.put(i,new ArrayList<>());
         }
-        List<Integer> finished = new ArrayList<>();
-        for(int course = 0; course < numCourses; course++){
-            if(prerequisite[course] == 0){
-                finished.add(course);
-            }
+        
+        for(int[] a:pre)
+        {
+            int v1=a[0];
+            int v2=a[1];
+            map.get(v2).add(v1);
         }
-        List<Integer> NULL = new ArrayList<>();
-        for(int index = 0; index < finished.size(); index++){
-            for(int course : graph.getOrDefault(finished.get(index), NULL)){
-                if(--prerequisite[course] == 0){
-                    finished.add(course);
-                }
-            }
-        }
-        return finished.size() == numCourses;
+        
+        return TopoSort(map);
     }
+    
+    private boolean TopoSort(HashMap<Integer,List<Integer>> map)
+    {
+        int in[]=indegree(map);
+        Queue<Integer> q=new LinkedList<>();
+
+        for(int i=0;i<in.length;i++)
+            if(in[i]==0)
+                q.add(i);
+        int c=0;
+        while(!q.isEmpty())
+        {
+            int key=q.peek();
+            q.remove();
+            c++;
+            for(int nbrs: map.get(key))
+            {
+                in[nbrs]--;
+                if(in[nbrs]==0)
+                    q.add(nbrs);
+
+            }
+        }
+        
+        return c==map.size()?true : false;
+    }
+    
+     private int[] indegree(HashMap<Integer,List<Integer>> map){
+        int[] in=new int[map.size()];
+        for(int key:map.keySet())
+        {
+            for(int nbrs:map.get(key))
+                in[nbrs]++;
+        }
+        return in;
+    }
+
+    
+    
+    
 }
