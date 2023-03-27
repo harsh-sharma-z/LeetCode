eb39f96ea161e561caf84ae7f520ddccbe011ab2
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2023-03-27 08:37:36 +0530

    Added solution - LeetHub

---
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class DriverClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+            int V = sc.nextInt();
+            int E = sc.nextInt();
+            for (int i = 0; i < V; i++)
+                list.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                int u = sc.nextInt();
+                int v = sc.nextInt();
+                list.get(u).add(v);
+            }
+            if (new Solution().isCyclic(V, list) == true)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*Complete the function below*/
+
+class Solution {
+    // Function to detect cycle in a directed graph.
+    public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
+        // code here
+        int[] vis=new int[V];
+        int[] path=new int[V];
+        
+        for(int i=0;i<V;i++)
+        if(vis[i]==0)
+        {
+            if (dfs(i, vis, path  , adj))
+                return true;
+        }
+                
+        return false;
+    }
+    
+    private boolean dfs(int src, int[] vis, int[] path , ArrayList<ArrayList<Integer>> adj)
+    {
+        vis[src]=1;
+        path[src]=1;
+        
+        for(int i: adj.get(src))
+        {
+            if(vis[i]==0)
+            {
+                if(dfs(i,vis,path,adj))
+                return true;
+            }
+            else if(path[i]==1)
+                return true;
+                
+        }
+        path[src]=0;
+        
+        return false;
+    }
+}
