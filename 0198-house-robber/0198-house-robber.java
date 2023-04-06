e412b7c4f47299037ce212bfb70680ceedd382f3
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2023-04-06 09:15:05 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (5.75%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,15 +1,27 @@
 class Solution {
     public int rob(int[] nums) {
-        int Y=0;
-        int N=0;
-        
-        for(int i:nums)
-        {
-            int temp=N;
-            N=Math.max(Y,N);
-            Y=i+temp;
-        }
-        return Math.max(Y,N);
+        int[] dp=new int[nums.length];
+        Arrays.fill(dp,-1);
+        return robberTD(nums,0,dp);  
     }
- 
+    // private int robber(int[] nums, int i)
+    // {
+    //     if(i>=nums.length)
+    //         return 0;
+    //     int yes=nums[i]+robber (nums,i+2);
+    //     int no=robber(nums, i+1);
+    //     return Math.max(yes,no);
+    // }
+    
+    private int robberTD(int[] nums, int i,int[] dp)
+    {
+        if(i>=nums.length)
+            return 0;
+        if(dp[i]!=-1)
+            return dp[i];
+        int yes=nums[i]+robberTD (nums,i+2,dp);
+        int no=robberTD(nums, i+1,dp);
+        return dp[i]=Math.max(yes,no);
+    }
+    
 }
