6effc3708ec555fa35bd29e6ed7d109639267219
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2023-04-09 18:33:33 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.java
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.java
@@ -33,58 +33,88 @@ public class Main{
 
 // User function Template for Java
 
-class Solution{
-    static class Pair{
-        int node;
-        int distance;
-        public Pair(int distance, int node)
+class DisjointSet{
+    List<Integer> parent=new ArrayList<>();
+    List<Integer> rank=new ArrayList<>();
+    public DisjointSet(int n)
+    {
+        for(int i=0;i<=n;i++)
         {
-            this.distance=distance;
-            this.node=node;
+            rank.add(0);
+            parent.add(i); // sab apne parent
         }
     }
+
+    public int findPar(int node)
+    {
+        if(node==parent.get(node)) //agar vo khudka parent hai
+            return node;
+        int ultpar=findPar(parent.get(node)); //recursively ultimate boss find karo
+        parent.set(node,ultpar); // lage haath parent update kardo rast evalo ke bhi
+        return parent.get(node); // return kardo updated list ki parent value
+    }
+
+    public void unionByRank(int u , int v)
+    {
+        int ult_u=findPar(u);
+        int ult_v=findPar(v);
+        if(ult_v==ult_u) // agar dono ke papa same hai to kuch nhi karne ka
+            return ;
+        if(rank.get(ult_u)<rank.get(ult_v)) // u chhota h v se attach
+        {
+            parent.set(ult_u,ult_v);
+        }
+        else if(rank.get(ult_v)<rank.get(ult_u)) // v chhota hai to u se attach
+        {
+            parent.set(ult_v,ult_u);
+        }
+        else {  // varna koi bhi kisi se bhi attach but rank badhana padega
+            parent.set(ult_u,ult_v);
+            int rankU=rank.get(ult_u);
+            rank.set(ult_u,rankU+1);
+        }
+    }
+}
+class Edge implements Comparable<Edge>{
+    int src, dest, w;
+    public Edge(int src, int dest, int w)
+    {
+        this.src=src;
+        this.dest=dest;
+        this.w=w;
+    }
+    public int compareTo(Edge compareEdge)
+    {
+        return this.w-compareEdge.w;
+    }
+};
+class Solution{
 	static int spanningTree(int V, int E, int edges[][]){
 	    // Code Here. 
-	    ArrayList<ArrayList<Pair>> adj=new ArrayList<>();
-	    for(int i=0;i<V;i++)
-	    adj.add(new ArrayList<Pair>());
-	    
-	    for(int i[]:edges)
+	    List<Edge> uwu=new ArrayList<>();
+	    for(int[] i :edges)
 	    {
 	        int u=i[0];
 	        int v=i[1];
-	        int d=i[2];
-	        adj.get(u).add(new Pair(d,v));
-	        adj.get(v).add(new Pair(d,u));
+	        int cost=i[2];
+	        Edge temp= new Edge(u,v,cost);
+	        uwu.add(temp);
 	    }
-	    
-	    int sum=0;
-	    
-	    int[] vis=new int[V];
-	   
-	    
-	    PriorityQueue<Pair> uwu=new PriorityQueue<Pair>((x,y)->x.distance-y.distance);
-	    uwu.add(new Pair(0,0));
-	    
-	    while(!uwu.isEmpty())
+	    DisjointSet ds=new DisjointSet(V);
+	    Collections.sort(uwu);
+	    int ans=0;
+	    for(Edge e: uwu)
 	    {
-	        int node=uwu.peek().node;
-	        int dis=uwu.peek().distance;
-	        uwu.poll();
-	        if(vis[node]==1)
-	        continue;
-	        sum+=dis;
-	        vis[node]=1;
+	        int w=e.w;
+	        int u=e.src;
+	        int v=e.dest;
 	        
-	        for(Pair i:adj.get(node))
+	        if(ds.findPar(u)!=ds.findPar(v))
 	        {
-	            int newnode=i.node;
-	            int newdis=i.distance;
-	            
-	            if(vis[newnode]==0)
-	            uwu.add(new Pair(newdis, newnode));
+	            ans+=w;
+	            ds.unionByRank(u,v);
 	        }
 	    }
-	    return sum;
+	    return ans;
 	}
 }
