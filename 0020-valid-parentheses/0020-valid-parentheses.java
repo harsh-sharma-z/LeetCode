406cb496431fc87a280d0bebfa36299bda3bcc16
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2023-04-10 08:47:15 +0530

    Time: 2 ms (79.07%), Space: 40.5 MB (63.52%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> uwu=new Stack<>();
+        for(char c: s.toCharArray())
+        {
+            if(c=='{')
+                uwu.push('}');
+            else if(c=='(')
+                uwu.push(')');
+            else if(c=='[')
+                uwu.push(']');
+            else if(uwu.isEmpty() || uwu.pop()!=c)
+                return false;
+        }
+        if(uwu.isEmpty())
+        return true;
+        return false;
+    }
+}
