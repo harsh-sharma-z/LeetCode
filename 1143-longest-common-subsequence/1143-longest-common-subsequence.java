6a3c9f780e65d786ccc6139c56782b867426279d
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2023-04-13 10:04:26 +0530

    Time: 30 ms (41.00%), Space: 50.3 MB (58.13%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,8 +1,16 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
+        
+        
         //return lcs(text1,text2,0,0);
+        
+        
         //return lcs2(text1,text2,text1.length()-1, text2.length()-1);
+        
+        
         return lcsBU(text1,text2);
+        
+        
 //         int[][] dp=new int[text1.length()+1][text2.length()+1];
         
 //         for(int[] a:dp)
@@ -11,6 +19,10 @@ class Solution {
 //         return lcsTD(text1, text2, 0,0, dp);
         
     }
+    
+    
+    
+    
     private int lcs (String s1, String s2, int i, int j)
     {
         if(i==s1.length() || j==s2.length())
@@ -27,6 +39,10 @@ class Solution {
         }
         return ans;
     }
+    
+    
+    
+    
     private int lcs2 (String s1, String s2, int i, int j)
     {
         if(i<0 || j<0)
@@ -42,6 +58,10 @@ class Solution {
         }
         return ans;
     }
+    
+    
+    
+    
     private int lcsTD (String s1, String s2, int i, int j,int[][] dp)
     {
         if(i==s1.length() || j==s2.length())
@@ -59,25 +79,29 @@ class Solution {
         }
         return dp[i][j]=ans;
     }
+    
+    
+    
+    
     private int lcsBU (String s1, String s2)
     {
         int[][] dp=new int[s1.length()+1][s2.length()+1];
-        for(int i=1;i<dp.length;i++)
+        for(int i=s1.length()-1;i>=0;i--)
         {
-            for(int j=1;j<dp[0].length;j++)
+            for(int j=s2.length()-1;j>=0;j--)
             {
                 int ans=0;
-                if(s1.charAt(i-1)==s2.charAt(j-1))
-                    ans=1+dp[i-1][j-1];
+                if(s1.charAt(i)==s2.charAt(j))
+                    ans=1+dp[i+1][j+1];
                 else
                 {
-                    int fs=dp[i-1][j];
-                    int ss=dp[i][j-1];
+                    int fs=dp[i+1][j];
+                    int ss=dp[i][j+1];
                     ans=Math.max(fs,ss);
                 }
                 dp[i][j]=ans;
             }
         }
-        return dp[dp.length-1][dp[0].length-1];
+        return dp[0][0];
     }
 }
