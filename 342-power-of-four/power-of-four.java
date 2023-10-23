6ade17b14f9d2b9922060a313c297c2dfd000310
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2023-10-23 22:09:25 +0530

    Time: 0 ms (100.00%) | Memory: 39.1 MB (91.80%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -1,11 +1,17 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
 
-        //loop vala logic
-        if(n == 0)
+        // //loop vala logic
+        // if(n == 0)
+        //     return false;
+        // while(n % 4 == 0)
+        //     n /= 4;
+        // return n == 1;
+
+        //log n vala logic
+        if(n <= 0)
             return false;
-        while(n % 4 == 0)
-            n /= 4;
-        return n == 1;
+        int x = (int)(Math.log(n) / Math.log(4));
+        return Math.pow(4 , x) == n;
     }
 }
