fda502bd2e625bd6f3f9f9bf445f2b422ddd8fab
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2023-10-23 22:12:14 +0530

    Time: 0 ms (100.00%) | Memory: 39.8 MB (25.73%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -8,10 +8,15 @@ class Solution {
         //     n /= 4;
         // return n == 1;
 
-        //log n vala logic
+        // //log n vala logic
+        // if(n <= 0)
+        //     return false;
+        // int x = (int)(Math.log(n) / Math.log(4));
+        // return Math.pow(4 , x) == n;
+
+        //Bit magic vala logic
         if(n <= 0)
             return false;
-        int x = (int)(Math.log(n) / Math.log(4));
-        return Math.pow(4 , x) == n;
+        return (((n & (n - 1)) == 0) && ((n - 1) % 3 == 0));
     }
 }
