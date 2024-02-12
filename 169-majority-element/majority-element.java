621712b0ee8b11039860049fa42eb50348f51a46
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2024-02-12 16:24:07 +0530

    Time: 2 ms (78.11%) | Memory: 50.5 MB (47.42%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,10 +1,10 @@
 class Solution {
     public int majorityElement(int[] nums) {
         // Boyre Moore's Algorithm
-        int c = 0 , ans = 0;
+        int c = 0, ans = 0; //assumption of the answer
         for(int i : nums){
             if(c == 0) ans = i;
-            c = ans == i ? c + 1 : c - 1;
+            c = i == ans ? c + 1 : c - 1;
         }
         return ans;
     }
