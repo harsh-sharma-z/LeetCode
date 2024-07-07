44796f7edd1693e2fd42dc9afedb8821dfa9aeea
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2024-07-07 22:11:45 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (21.39%) - LeetSync

--- 1642-water-bottles/water-bottles.java
+++ 1642-water-bottles/water-bottles.java
@@ -1,12 +1,5 @@
 class Solution {
     public int numWaterBottles(int numBottles, int numExchange) {
-        int ans = numBottles;
-        while(numBottles >= numExchange) {
-            int rem = numBottles % numExchange;
-            numBottles /= numExchange;
-            ans += numBottles;
-            numBottles += rem;
-        }
-        return ans;
+        return numBottles + (numBottles - 1) / (numExchange - 1);
     }
 }
