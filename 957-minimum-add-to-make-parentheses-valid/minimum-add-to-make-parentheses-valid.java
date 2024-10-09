d7f6aa13c7029cdf68deeef5f260207fae84e456
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2024-10-09 23:09:33 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (8.99%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int left = 0 , right = 0;
+        for(int i = 0 ; i < s.length() ; i++) {
+            char c = s.charAt(i);
+            if(c == '(') {
+                right++;
+            }
+            else if (right > 0) {
+                right--;
+            }
+            else {
+                left++;
+            }
+        }
+
+        return left + right;
+    }
+}
