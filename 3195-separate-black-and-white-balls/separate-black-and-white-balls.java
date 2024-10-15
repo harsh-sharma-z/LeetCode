3e715ab23e534b33e45b13fdc535d5915543488e
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2024-10-15 22:04:33 +0530

    Time: 7 ms (100.00%) | Memory: 45.7 MB (20.43%) - LeetSync

---
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long minimumSteps(String s) {
+        long cnt = 0, ans = 0;
+        for(int i = s.length() - 1; i >= 0; i--) {
+            if(s.charAt(i) == '1') {
+                ans += cnt;
+            } else {
+                cnt++;
+            }
+        }
+        return ans;
+    }
+}
