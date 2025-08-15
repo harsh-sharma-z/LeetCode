0709f9488d9b2478c8303e46009018f3cf601c64
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2025-08-15 23:44:39 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (77.96%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -1,22 +1,34 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
+        // AAJ aap log seekhoge power of four nikalna
+        // bkl diwakar dekhle seekhle placement hojayega
 
-        // //loop vala logic
-        // if(n == 0)
-        //     return false;
-        // while(n % 4 == 0)
-        //     n /= 4;
-        // return n == 1;
-
-        // //log n vala logic
-        // if(n <= 0)
-        //     return false;
-        // int x = (int)(Math.log(n) / Math.log(4));
-        // return Math.pow(4 , x) == n;
-
-        //Bit magic vala logic
-        if(n <= 0)
+        //Pehle m krnga loop vale logic se
+         
+         /*
+        if(n == 0) 
             return false;
-        return (((n & (n - 1)) == 0) && ((n - 1) % 3 == 0));
+        while(n % 4 == 0)
+            n /= 4;
+        return n == 1;
+        */
+        
+        // lekin ye upar vala logic jhantu hai to ab him ise krnge log n vale logic se
+
+        /*
+        if(n <=0 )
+            return false;
+        int x = (int)(Math.log(n) / Math.log(4));
+        return Math.pow(4 , x) == n;
+        */
+
+
+        //Pr agar tjhe interviewer ki kachhi geeli krni h to krna padega bit-magic
+
+        if(n <= 0) 
+            return false;
+        return (((n &(n - 1)) == 0) && ((n - 1) % 3 == 0));
+
+
     }
 }
